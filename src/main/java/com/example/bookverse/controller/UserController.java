2d884450352fb4ca1e03adc0ac45e238eda67412
Heeyeon An <gmldus4613@naver.com>
2024-12-31 13:03:07 +0900

    아이디 찾기, 비번 찾기, 예외처리 추가

--- src/main/java/com/example/bookverse/controller/UserController.java
+++ src/main/java/com/example/bookverse/controller/UserController.java
@@ -5,10 +5,7 @@ import com.example.bookverse.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,5 +19,17 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.CREATED).body("User joined");
         };
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User not joined");
+    }
+
+    @GetMapping(value = "/find/id")
+    public ResponseEntity<String> findId(@RequestBody UserDTO user) {
+        String id = userService.findId(user);
+        return ResponseEntity.status(HttpStatus.OK).body(id);
+    }
+
+    @GetMapping(value = "/find/pw")
+    public ResponseEntity<String> findPw(@RequestBody UserDTO user) {
+        String newPw = userService.findPw(user);
+        return ResponseEntity.status(HttpStatus.OK).body(newPw);
     }
 }
--- src/main/java/com/example/bookverse/service/UserService.java
+++ src/main/java/com/example/bookverse/service/UserService.java
@@ -3,10 +3,15 @@ package com.example.bookverse.service;
 import com.example.bookverse.data.dto.UserDTO;
 import com.example.bookverse.data.entity.UserEntity;
 import com.example.bookverse.data.repository.UserRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.security.SecureRandom;
 import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
@@ -14,6 +19,9 @@ import java.time.LocalDateTime;
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private EntityManager entityManager;
 
     public UserEntity saveUser(UserDTO user) {
         if (this.userRepository.findByEmail(user.getEmail()) == null) {
@@ -31,4 +39,42 @@ public class UserService {
         }
         return null;
     }
+
+    public String findId(UserDTO user) {
+        String id = userRepository.findIdByEmail(user.getName(), user.getBirthdate(), user.getPhone());
+        if (id == null) {
+            throw new EntityNotFoundException("일치하는 회원이 없습니다. 다시 입력해주세요.");
+        }
+        return id;
+    }
+    
+    public String findPw(UserDTO user) {
+        UserEntity userEntity = userRepository.findUserEntityByEmailNameBirthdatePhone(user.getEmail(), user.getName(), user.getBirthdate(), user.getPhone());
+        if (userEntity == null) {
+            throw new EntityNotFoundException("일치하는 회원이 없습니다. 다시 입력해주세요.");
+        }
+
+        String newPw = createNewPw(10);
+        String encodePw = passwordEncoder.encode(newPw);
+        userEntity.setPw(encodePw);
+        userEntity.setUpdatedAt(LocalDateTime.now());
+        userRepository.save(userEntity);
+
+        return newPw;
+    }
+
+    public String createNewPw(int len) {
+        final String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        SecureRandom rm = new SecureRandom();
+        StringBuffer sb = new StringBuffer();
+
+        for(int i=0; i<len; i++) {
+            int index = rm.nextInt(chars.length());
+            sb.append(chars.charAt(index));
+        }
+        return sb.toString();
+    }
+
+
 }
