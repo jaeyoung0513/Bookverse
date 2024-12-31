eea62fcbb100e56eb93c122a429fb21554397596
Ryu Jaeyoung <rjykab@gmail.com>
2024-12-31 18:19:01 +0900

    재영

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+
+    return (
+        <div>
+            Footer입니다.
+        </div>
+    )
+}
---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+
+export default function MainLayout() {
+    return (
+        <>
+        <Header />
+        <div>
+        <Outlet />    
+        </div>
+        <Footer />
+        </>
+    )
+}
