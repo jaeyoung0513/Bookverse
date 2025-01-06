09406d17f54695d6337af405f56246b8d3d98d15
Ryu Jaeyoung <rjykab@gmail.com>
2025-01-07 00:15:45 +0900

    재영

--- src/components/common/Nav.jsx
+++ src/components/common/Nav.jsx
@@ -1,14 +1,16 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import style from "../../styles/Nav.module.css";
 import { useState } from "react";
+import SearchForm from "./SearchForm";
 
 export default function Nav() {
   const [searchForm, setSearchForm] = useState("");
+  const navigate = useNavigate(); // 페이지 네비게이션을 위해 useNavigate 훅 사용
 
   const handleSearch = (event) => {
     event.preventDefault(); // 페이지 리로드 방지
     console.log("Searching for:", searchForm);
-    // 검색 로직을 여기에 구현, 예: 페이지 리다이렉트 또는 API 호출
+    navigate(`/booklist/searchResult?query=${encodeURIComponent(searchForm)}`); // 검색어를 URL 파라미터로 포함하여 리다이렉트
   };
 
   return (
@@ -37,22 +39,7 @@ export default function Nav() {
         </NavLink>
       </div>
       <div className={style.rightContainer}>
-        <form className={style.searchForm} onSubmit={handleSearch}>
-          <input
-            type="text"
-            placeholder="제목, 작가를 입력하세요."
-            value={searchForm}
-            onChange={(e) => setSearchForm(e.target.value)}
-            className={style.searchInput}
-          />
-          <button type="submit" className={style.searchButton}>
-            <img
-              src="/assets/searchIcon.png"
-              alt="검색"
-              className={style.searchIcon}
-            />
-          </button>
-        </form>
+        <SearchForm />
         <NavLink to="/mymenu/cart">
           <img
             src="/assets/cart.png"
